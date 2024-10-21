94cdb3b86a4f527b757c5b466393bda7252f0d16
Florin-Robert Pap <165192808+RobertPap57@users.noreply.github.com>
2024-10-21 15:20:53 +0200

    second commit

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,19 +1,64 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    PlayerComponent,
+    MatIconModule,
+    MatButtonModule,
+   ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-  
-  pickCardAnimation = false
 
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+
+  }
 
   takeCard() {
-    this.pickCardAnimation= true
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() || '';
+      this.pickCardAnimation = true
+      console.log(this.game);
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.currentCard);
+      }, 1000);
+    }
   }
 }
+
+
+// openDialog(): void {
+//   const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+//     data: {name: this.name(), animal: this.animal()},
+//   });
+
+//   dialogRef.afterClosed().subscribe(result => {
+//     console.log('The dialog was closed');
+//     if (result !== undefined) {
+//       this.animal.set(result);
+//     }
+//   });
+// }
+// }
